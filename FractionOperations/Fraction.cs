3260a1d045453bafb8d8c77f438dbf8e1d9324da
alextim1 <alexey.timohin@gmail.com>
2016-02-16 13:23:43 +0200

    fraction operations

---
+++ FractionOperations/Fraction.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FractionOperations
+{
+    public interface IFraction
+    {
+        int Numerator { get; }
+        int Denominator { get; }
+        void OptimizeFraction();
+    }
+
+
+    public class Fraction : IFraction
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public Fraction(string fraction)
+        {
+            string[] fractionParts = fraction.Split('/');
+            
+            if(!((fractionParts.Length==1)||(fractionParts.Length==2)))
+            {
+                throw new ArgumentException();
+            }
+
+            if (fractionParts.Length == 1)
+            {
+                try
+                {
+                    _numerator = int.Parse(fractionParts[0]);
+
+                    _denominator = 1;
+
+                }
+
+                catch (Exception e)
+                {
+                    throw new ArgumentException();
+                }
+
+                _denominator = 1;
+            }
+
+            else
+            {
+                try
+                {
+                    _numerator = int.Parse(fractionParts[0]);
+                    _denominator = int.Parse(fractionParts[1]);
+
+                }
+
+                catch (Exception e)
+                {
+                    throw new ArgumentException();
+                }
+            }
+            
+
+            if (_denominator==0)
+            {
+                throw new ArgumentException();
+            }
+
+            OptimizeFraction();
+        }
+
+        public override string ToString()
+        {
+            return (_numerator.ToString()+"/"+_denominator.ToString());
+        }
+
+        public int Denominator
+        {
+            get
+            {
+                return _denominator;
+            }
+        }
+
+        public int Numerator
+        {
+            get
+            {
+                return _numerator;
+            }
+        }
+
+        private int GCD(int a, int b)
+        {
+            if (a == 0)
+                return b;
+
+            if (b == 0)
+                return Math.Abs(a);
+
+            return GCD(b, a % b);
+        }
+
+        public void OptimizeFraction()
+        {
+            int gcd = GCD(_numerator, _denominator);
+            _numerator = _numerator / gcd;
+            _denominator = _denominator / gcd;
+
+            if ((_numerator<0)&&(_denominator<0))
+            {
+                _numerator = Math.Abs(_numerator);
+                _denominator = Math.Abs(_denominator);
+            }
+
+            if (_denominator<0)
+            {
+                _numerator = -(_numerator);
+                _denominator = Math.Abs(_denominator);
+            }
+        }
+    }
+
+
+    public static class FractionCalculator
+    {
+        public static Fraction Add(Fraction arg1, Fraction arg2)
+        {
+            Fraction result = new Fraction((arg1.Numerator * arg2.Denominator + arg2.Numerator * arg1.Denominator).ToString() + "/" + (arg1.Denominator * arg2.Denominator).ToString());
+            result.OptimizeFraction();
+            return result;
+        }
+
+        public static Fraction Mul(Fraction arg1, Fraction arg2)
+        {
+            Fraction result = new Fraction((arg1.Numerator * arg2.Numerator).ToString() + "/" + (arg1.Denominator * arg2.Denominator).ToString());
+            result.OptimizeFraction();
+            return result;
+        }
+
+        public static Fraction Sub(Fraction arg1, Fraction arg2)
+        {
+            Fraction minusOne = new Fraction("-1/1");
+            Fraction result = (Add(arg1, Mul(minusOne, arg2)));
+            result.OptimizeFraction();
+            return result;
+        }
+
+        public static Fraction Div(Fraction arg1, Fraction arg2)
+        {
+            if (arg2.Numerator == 0)
+                throw new DivideByZeroException();
+
+            Fraction result = new Fraction((arg1.Numerator*arg2.Denominator).ToString()+"/"+(arg1.Denominator*arg2.Numerator).ToString());
+            result.OptimizeFraction();
+            return result;
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FractionOperations;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InputException1()
+        {
+            //arrange
+
+            Fraction test = new Fraction("11a/11");
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InputException2()
+        {
+            //arrange
+
+            Fraction test = new Fraction("11/1a1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InputException3()
+        {
+            //arrange
+
+            Fraction test = new Fraction("11/0");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InputException4()
+        {
+            //arrange
+
+            Fraction test = new Fraction("11/11/1");
+        }
+
+
+        [TestMethod]
+       
+        public void InputCorrect1()
+        {
+            //arrange
+
+            Fraction test = new Fraction("11");
+
+            //assert
+            Assert.AreEqual(11, test.Numerator);
+            Assert.AreEqual(1, test.Denominator);
+        }
+
+        [TestMethod]
+
+        public void InputCorrect2()
+        {
+            //arrange
+
+            Fraction test = new Fraction("-11/-12");
+
+            //assert
+            Assert.AreEqual(11, test.Numerator);
+            Assert.AreEqual(12, test.Denominator);
+        }
+
+        [TestMethod]
+
+        public void InputCorrect3()
+        {
+            //arrange
+
+            Fraction test = new Fraction("11/-12");
+
+            //assert
+            Assert.AreEqual(-11, test.Numerator);
+            Assert.AreEqual(12, test.Denominator);
+        }
+
+        [TestMethod]
+
+        public void InputCorrect4()
+        {
+            //arrange
+
+            Fraction test = new Fraction("-11/12");
+
+            //assert
+            Assert.AreEqual(-11, test.Numerator);
+            Assert.AreEqual(12, test.Denominator);
+        }
+
+        [TestMethod]
+
+        public void InputCorrect5()
+        {
+            //arrange
+
+            Fraction test = new Fraction("0/11");
+
+            //assert
+            Assert.AreEqual(0, test.Numerator);
+            Assert.AreEqual(1, test.Denominator);
+        }
+
+
+        [TestMethod]
+
+        public void InputCorrect6()
+        {
+            //arrange
+
+            Fraction test = new Fraction("6/12");
+
+            //assert
+            Assert.AreEqual(1, test.Numerator);
+            Assert.AreEqual(2, test.Denominator);
+        }
+
+
+        [TestMethod]
+
+        public void CalculationValid1()
+        {
+            //arrange
+
+            Fraction test = new Fraction("1/3");
+            Fraction test2 = new Fraction("1/2");
+
+            //act
+
+            var result = FractionCalculator.Add(test,test2);
+           
+            
+            //assert
+            Assert.AreEqual(5, result.Numerator);
+            Assert.AreEqual(6, result.Denominator);
+        }
+
+
+        [TestMethod]
+
+        public void CalculationValid2()
+        {
+            //arrange
+
+            Fraction test = new Fraction("1/3");
+            Fraction test2 = new Fraction("1/2");
+
+            //act
+
+            var result = FractionCalculator.Sub(test, test2);
+
+
+            //assert
+            Assert.AreEqual(-1, result.Numerator);
+            Assert.AreEqual(6, result.Denominator);
+        }
+
+
+        [TestMethod]
+
+        public void CalculationValid3()
+        {
+            //arrange
+
+            Fraction test = new Fraction("2/3");
+            Fraction test2 = new Fraction("3/2");
+
+            //act
+
+            var result = FractionCalculator.Mul(test, test2);
+
+
+            //assert
+            Assert.AreEqual(1, result.Numerator);
+            Assert.AreEqual(1, result.Denominator);
+        }
+
+
+        [TestMethod]
+
+        public void CalculationValid4()
+        {
+            //arrange
+
+            Fraction test = new Fraction("-1/3");
+            Fraction test2 = new Fraction("-1/2");
+
+            //act
+
+            var result = FractionCalculator.Div(test, test2);
+
+
+            //assert
+            Assert.AreEqual(2, result.Numerator);
+            Assert.AreEqual(3, result.Denominator);
+        }
+
+
+        [TestMethod]
+
+        [ExpectedException(typeof(DivideByZeroException))]
+
+        public void CalculationInValid1()
+        {
+            //arrange
+
+            Fraction test = new Fraction("1/3");
+            Fraction test2 = new Fraction("0");
+
+            //act
+
+            var result = FractionCalculator.Div(test, test2);
+
+
+            //assert
+            
+        }
+    }
+}
